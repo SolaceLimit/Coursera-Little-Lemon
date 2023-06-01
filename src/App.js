4da837f646bcdc5f5b430352f70abcd3359e7a89
SolaceLimit <Nathan_Eghbali@yahoo.com>
2023-06-01 10:13:29 -0500

    added form validation and fields

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <>
-    <Header />
-    <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { AlertProvider } from "./context/alertContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const helmetContext = {};
 root.render(
-  <React.StrictMode>
-    <HelmetProvider context={helmetContext}>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<HelmetProvider context={helmetContext}>
+			<BrowserRouter>
+				<AlertProvider>
+					<App />
+				</AlertProvider>
+			</BrowserRouter>
+		</HelmetProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
