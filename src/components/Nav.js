585bc22f8c0aa9a42cd710a9522f849313827110
SolaceLimit <Nathan_Eghbali@yahoo.com>
2023-06-17 06:30:50 -0500

    updated navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,58 @@
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
 import { Routes, Route, Link } from "react-router-dom";
-import HomePage  from "./Home";
-import ContactPage  from "./Contact";
-import ReservationPage  from "./Reservation";
+import HomePage from "./Home";
+import ContactPage from "./Contact";
+import ReservationPage from "./Reservation";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { AppstoreOutlined } from "@ant-design/icons";
+
+const items = [
+	{
+		label: (
+			<Link to='/' className='nav-item'>
+				Home
+			</Link>
+		),
+		key: "alipay",
+		icon: <AppstoreOutlined />,
+	},
+	{
+		label: (
+			<Link to='/reservation' className='nav-item'>
+				Reservation
+			</Link>
+		),
+		key: "alipay",
+		icon: <AppstoreOutlined />,
+	},
+	{
+		label: (
+			<Link to='/contact' className='nav-item'>
+				Contact
+			</Link>
+		),
+		key: "alipay",
+		icon: <AppstoreOutlined />,
+	},
+];
 
 const Nav = () => {
-  return (
-  <>
-    <nav>
-        <Flex>  
-          <Box w='100' h='10' bg='white'><Link to="/" className="nav-item">Home</Link></Box>
-          <Box w='100' h='10' bg='white'><Link to="/reservation" className="nav-item">Reservation</Link></Box>
-          <Box w='100' h='10' bg='white'><Link to="/contact" className="nav-item">Contact</Link></Box>
-        </Flex>
-    </nav>
-    <Routes> 
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/reservation" element={<ReservationPage />}></Route>
-          <Route path="/contact" element={<ContactPage />}></Route>
-    </Routes>
-  </>
-  );
+	return (
+		<>
+			<nav>
+				<Menu
+					theme='dark'
+					mode='horizontal'
+					defaultSelectedKeys={["2"]}
+					items={items}
+				/>
+			</nav>
+			<Routes>
+				<Route path='/' element={<HomePage />}></Route>
+				<Route path='/reservation' element={<ReservationPage />}></Route>
+				<Route path='/contact' element={<ContactPage />}></Route>
+			</Routes>
+		</>
+	);
 };
 export default Nav;
